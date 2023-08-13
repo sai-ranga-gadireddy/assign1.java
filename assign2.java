e4e5d28f8cb82ee7d4b9cea9d5d81addcf87b54d
sai-ranga-gadireddy <140521382+sai-ranga-gadireddy@users.noreply.github.com>
2023-08-13 12:19:52 +0530

    Create assign2.java

---
+++ assign2.java
@@ -0,0 +1,26 @@
+public class Primeno 
+{
+public static void main(String[] args) 
+{ 
+  int num=27;
+  checkPrime(num);
+}
+private static void checkPrime(int num)
+  {
+    int cnt=0;
+    if(num<2)
+    {
+      System.out.println(num + " is not a prime number");
+      System.exit(0);
+    }
+    for(int i=1;i <=n;i++)
+      {
+        if(n%i==0)
+          cnt +=1;
+      }
+    if(cnt>2)
+      System.out.println(num + " is not a prime number");
+    else
+      System.out.println(num + " is a prime number");
+  }
+}
